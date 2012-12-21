d85f4d1566e23b3fd9b6056daf6ef6da0d4599b0
Tim Stirrat <tim.stirrat@gmail.com>
2012-12-21 16:00:54 +1100

    Added DIFF_SEARCH_RESULTS service and ui hooks

--- src/main/java/org/stirrat/ecm/components/diffutils/DiffServices.java
+++ src/main/java/org/stirrat/ecm/components/diffutils/DiffServices.java
@@ -38,12 +38,14 @@ public class DiffServices {
    * @throws ServiceException
    */
   @ServiceMethod(name = "DIFF_REVISIONS", template = "TPL_DIFF_RESULT")
-  public String diffRevisions(@Binder(name = "id1") long id1, @Binder(name = "id2") long id2, Service service)
+  public void diffRevisions(@Binder(name = "id1") long id1, @Binder(name = "id2") long id2, Service service)
       throws DataException, ServiceException {
     String file1 = getFileNameFromRevision(id1, service);
     String file2 = getFileNameFromRevision(id2, service);
 
-    return diffFiles(file1, file2, service.getBinder());
+    List<String> uDiff = diffFiles(file1, file2, service.getBinder());
+
+    putDiffInBinder(uDiff, service.getBinder());
   }
 
   /**
@@ -56,7 +58,7 @@ public class DiffServices {
    * @throws ServiceException
    */
   @ServiceMethod(name = "DIFF_EXTERNAL", template = "TPL_DIFF_RESULT")
-  public String diffExternal(@Binder(name = "dID") long dID, @Binder(name = "provider") String providerName,
+  public void diffExternal(@Binder(name = "dID") long dID, @Binder(name = "provider") String providerName,
       Service service) throws ServiceException, DataException {
 
     Provider provider = Providers.getProvider(providerName);
@@ -87,29 +89,115 @@ public class DiffServices {
     String remoteFile = responseBinder.getLocal("downloadFile:path");
 
     if (remoteFile == null) {
-      return remoteFile;
+      return;
     }
 
-    return diffFiles(remoteFile, localFile, service.getBinder());
+    List<String> uDiff = diffFiles(remoteFile, localFile, service.getBinder());
+
+    putDiffInBinder(uDiff, service.getBinder());
   }
 
   /**
-   * Diff two files and output the result into diffOutput and diffHtml binder
-   * variables.
+   * Diff each item returned in a search query with the same items found via an
+   * outgoing provider.
+   * 
+   * @param QueryText
+   * @param provider
+   * @return
+   * @throws DataException
+   * @throws ServiceException
+   */
+  @ServiceMethod(name = "DIFF_SEARCH_RESULTS", template = "TPL_DIFF_RESULT")
+  public void diffSearchResults(@Binder(name = "provider") String providerName, Service service)
+      throws ServiceException, DataException {
+
+    Provider provider = Providers.getProvider(providerName);
+
+    if (provider == null || provider.isInError()) {
+      throw new DataException("Provider '" + providerName + "' is not found or in error");
+    }
+
+    if (!provider.isProviderOfType("outgoing")) {
+      throw new ServiceException("Provider '" + provider.getName() + "' must be an outgoing provider");
+    }
+
+    service.executeSafeServiceInNewContext("GET_SEARCH_RESULTS", false);
+
+    DataResultSet rs = (DataResultSet) service.getBinder().getResultSet("SearchResults");
+
+    if (rs == null) {
+      throw new ServiceException("No SearchResults result set.");
+    }
+
+    List<String> allDiffs = new LinkedList<String>();
+
+    if (rs.getNumRows() > 0 && rs.first()) {
+
+      do {
+        long dID = Long.parseLong(rs.getStringValueByName("dID"));
+        String dDocName = rs.getStringValueByName("dDocName");
+
+        String localFile = getFileNameFromRevision(dID, service);
+
+        DataBinder requestBinder = new DataBinder();
+        DataBinder responseBinder = new DataBinder();
+
+        requestBinder.putLocal("IdcService", "GET_FILE");
+        requestBinder.putLocal("dDocName", dDocName);
+        requestBinder.putLocal("RevisionSelectionMethod", "Latest");
+        ServerRequestUtils.doAdminProxyRequest(provider, requestBinder, responseBinder, service);
+
+        if (responseBinder.getLocal("downloadFile:path") == null) {
+          throw new ServiceException("Unable to determine latest revision of '" + dDocName
+              + "' using outgoing provider '" + provider.getName());
+        }
+
+        String remoteFile = responseBinder.getLocal("downloadFile:path");
+
+        if (remoteFile == null) {
+          allDiffs.add("--- not found");
+          allDiffs.add("+++ " + localFile);
+        } else {
+
+          List<String> diff = diffFiles(remoteFile, localFile, service.getBinder());
+
+          if (diff.size() > 0) {
+            allDiffs.addAll(diff);
+
+          } else {
+            allDiffs.add("--- " + remoteFile);
+            allDiffs.add("+++ " + localFile);
+            allDiffs.add("@@ no changes @@");
+          }
+
+        }
+
+        allDiffs.add(" "); // empty line for spacing between diffs
+
+      } while (rs.next());
+    }
+
+    putDiffInBinder(allDiffs, service.getBinder());
+  }
+
+  /**
+   * Diff two files and return the line by line diffs.
    * 
    * @param file1
    * @param file2
    * @param binder
    * @return
    */
-  private String diffFiles(String file1, String file2, DataBinder binder) {
+  private List<String> diffFiles(String file1, String file2, DataBinder binder) {
     List<String> file1Content = fileToLines(file1);
     List<String> file2Content = fileToLines(file2);
 
     Patch patch = DiffUtils.diff(file1Content, file2Content);
 
-    List<String> uDiff = DiffUtils.generateUnifiedDiff(file1, file2, file1Content, patch, DEFAULT_CONTEXT_LINE_COUNT);
+    return DiffUtils.generateUnifiedDiff(file1, file2, file1Content, patch, DEFAULT_CONTEXT_LINE_COUNT);
+  }
 
+  private StringBuilder putDiffInBinder(List<String> uDiff, DataBinder binder) {
     StringBuilder diffOutput = new StringBuilder();
     StringBuilder diffHtml = new StringBuilder();
 
@@ -137,7 +225,7 @@ public class DiffServices {
 
     binder.putLocal("diffOutput", diffOutput.toString());
     binder.putLocal("diffHtml", diffHtml.toString());
-    return diffOutput.toString();
+    return diffOutput;
   }
 
   /**
